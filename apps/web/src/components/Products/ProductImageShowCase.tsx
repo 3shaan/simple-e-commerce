6c85c0c138511afd7ccda95e8886e2e2727ff463
3shaan <3shaan.dev@proton.me>
2025-04-23 22:39:02 +0600

    image shows with swiper

--- apps/web/src/components/Products/ProductImageShowCase.tsx
+++ apps/web/src/components/Products/ProductImageShowCase.tsx
@@ -2,9 +2,16 @@
 import Image from "next/image";
 import { useState } from "react";
 import ImageZoom from "./ImageZoom";
+// Import Swiper components and styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const ProductImageShowCase = () => {
-  const [activeSlide, setActiveSlide] = useState(2);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   const productImages = [
     {
@@ -33,63 +40,58 @@ const ProductImageShowCase = () => {
     },
   ];
 
-  const handleSlideChange = (id: number) => {
-    setActiveSlide(id);
-  };
-  console.log(activeSlide);
-  const activeImage = productImages.find((img) => img.id === activeSlide);
-  console.log(activeImage);
   return (
     <div className="w-full md:w-1/2 lg:w-5/12 xl:w-1/3 px-3 mb-6">
-      {/* <ImageZoom
-        src={activeImage?.image}
-        alt={`product-tab-${activeSlide}`}
-        width={100}
-        height={100}
-      /> */}
-      <div className=" h-full">
-        <div className=" sticky top-[30px]">
-          <div className=" mb-[15px]">
-            <div className="">
-              <div className="  h-full flex items-center text-center border border-solid border-[#e9e9e9] bg-[#f7f7f8] rounded-[5px] cursor-pointer">
-                <ImageZoom
-                  src={activeImage?.image}
-                  alt={`product-tab-${activeSlide}`}
-                  className="product-image w-full block m-auto"
-                  width={1000}
-                  height={1000}
-                />
-              </div>
-            </div>
+      <div className="h-full">
+        <div className="sticky top-[30px]">
+          <div className="mb-[15px]">
+            <Swiper
+              spaceBetween={10}
+              thumbs={{ swiper: thumbsSwiper }}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="product-images-slider"
+            >
+              {productImages.map((img) => (
+                <SwiperSlide key={img.id}>
+                  <div className="flex items-center text-center border border-solid border-[#e9e9e9] bg-[#f7f7f8] rounded-[5px]">
+                    <ImageZoom
+                      src={img.image}
+                      alt={`product-${img.id}`}
+                      className="product-image w-full block m-auto"
+                      width={1000}
+                      height={1000}
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
-          <div className="slider slider-nav thumb-image mx-[-6px] flex flex-wrap">
-            {productImages.map((img, index) => (
-              <div
-                key={img.id}
-                className="thumbnail-image cursor-pointer"
-                onClick={() => handleSlideChange(img.id)}
-                // onKeyDown={(e) => {
-                //   handleSlideChange(img.id);
-                // }}
-              >
-                <div
-                  className={`thumbImg mx-[6px] border border-solid ${
-                    activeSlide === img.id
-                      ? "border-[#64b496]"
-                      : "border-[#e9e9e9]"
-                  } rounded-[5px] flex justify-center items-center`}
-                >
-                  <Image
-                    src={img.image}
-                    alt={`product-tab-${img.id}`}
-                    className="w-full p-[2px] rounded-[5px]"
-                    width={100}
-                    height={100}
-                  />
+
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            spaceBetween={10}
+            slidesPerView={4}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="thumb-images-slider"
+          >
+            {productImages.map((img) => (
+              <SwiperSlide key={img.id}>
+                <div className="cursor-pointer">
+                  <div className="thumbImg border border-solid border-[#e9e9e9] rounded-[5px] flex justify-center items-center">
+                    <Image
+                      src={img.image}
+                      alt={`product-thumb-${img.id}`}
+                      className="w-full p-[2px] rounded-[5px]"
+                      width={100}
+                      height={100}
+                    />
+                  </div>
                 </div>
-              </div>
+              </SwiperSlide>
             ))}
-          </div>
+          </Swiper>
         </div>
       </div>
     </div>
