2de92c0e431a8fc711368849673b42387e3a646e
3shaan <3shaan.dev@proton.me>
2025-05-30 20:16:19 +0600

    api call from server side component is succesfull.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,14 +1,27 @@
+import ProductCategory from "@web/components/HomePage/Category/ProductCategory";
+import {
+  HydrateClient,
+  getQueryClient,
+  prefetch,
+  trpc,
+} from "@web/trpc/server";
 import HeroSlider from "../components/HomePage/HeroPage/HeroSlider";
 
-export default function Home() {
+export default async function Home() {
+  prefetch(trpc.menuRouter.findAll.queryOptions());
+  const queryClient = getQueryClient();
+  const menuList = await queryClient.fetchQuery(
+    trpc.menuRouter.findAll.queryOptions()
+  );
+  console.log(menuList);
   return (
-    <div>
+    <HydrateClient>
       <HeroSlider />
-      {/* <ProductCategory /> */}
+      <ProductCategory />
       {/* <PopulerProducts /> */}
       {/* <ProductsBanner /> */}
       {/* <ServicePage />
       // {/* <DealProducts /> */}
-    </div>
+    </HydrateClient>
   );
 }
--- apps/web/src/trpc/server.tsx
+++ apps/web/src/trpc/server.tsx
@@ -1,15 +1,49 @@
 import "server-only"; // <-- ensure this file cannot be imported from the client
 
+import { AppRouter } from "@server/trpc/trpc.route";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { createTRPCClient, httpLink } from "@trpc/client";
-import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import {
+  TRPCQueryOptions,
+  createTRPCOptionsProxy,
+} from "@trpc/tanstack-react-query";
 import { cache } from "react";
+import SuperJSON from "superjson";
 import { makeQueryClient } from "./query-client";
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
-export const trpc = createTRPCOptionsProxy({
-  client: createTRPCClient({
-    links: [httpLink({ url: "http://localhost:4000/trpc" })],
+
+export const trpc = createTRPCOptionsProxy<AppRouter>({
+  client: createTRPCClient<AppRouter>({
+    links: [
+      httpLink({
+        url: "http://localhost:4000/trpc",
+        transformer: SuperJSON,
+      }),
+    ],
   }),
   queryClient: getQueryClient,
 });
+
+// Hydrate the client on the server
+export function HydrateClient(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {props.children}
+    </HydrationBoundary>
+  );
+}
+
+// Prefetch the query on the server
+export function prefetch<T extends ReturnType<TRPCQueryOptions<any>>>(
+  queryOptions: T
+) {
+  const queryClient = getQueryClient();
+  if (queryOptions.queryKey[1]?.type === "infinite") {
+    void queryClient.prefetchInfiniteQuery(queryOptions as any);
+  } else {
+    void queryClient.prefetchQuery(queryOptions);
+  }
+}
